823181961369aee89d5ce0d4c9e7937d22e4f3e2
KiraElric <lissette.urso@gmail.com>
2021-05-10 21:23:10 -0400

    Adicion de los enums en el modelo Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :publisher
   has_and_belongs_to_many :authors
+
+  enum genre: %i[ drama novel fiction fantasy terror mistery science biography ]
+  enum lenguage: %i[ english spanish japanese germany portuguese french italian ]
 end
